ca1d97af0d611801582d667ba47c6b45be2d4d63
PavelBulaienko <p380504942472@gmail.com>
2021-03-14 19:32:36 +0200

    full done

--- js/index.js
+++ js/index.js
@@ -1,12 +1,14 @@
 import imgArr from '/js/gallery-items.js';
 
+
 const galleryList = document.querySelector('.gallery');
 
 imgArr.forEach(element => {
     const imgEl = document.createElement('img');
     const liEl = document.createElement('li');
-    imgEl.src = element.preview;
+
     imgEl.classList.add('gallery__image');
+    imgEl.src = element.preview;
     imgEl.alt = element.description;
     liEl.append(imgEl);
     galleryList.append(liEl);
@@ -15,6 +17,7 @@ imgArr.forEach(element => {
 
 const modal = document.querySelector('.lightbox')
 const modalImg = document.querySelector('.lightbox__image')
+let indexOfImg;
 
 
 galleryList.addEventListener('click', (event) => {
@@ -22,9 +25,11 @@ galleryList.addEventListener('click', (event) => {
     imgArr.forEach(img => {
         if (img.description === event.target.alt) {
             modalImg.src = img.original;
+            indexOfImg = imgArr.indexOf(img);
         }
     });
 })
+
 
 const closeBtn = document.querySelector('.lightbox__button');
 const lightboxOverlay = document.querySelector('.lightbox__overlay');
@@ -39,9 +44,43 @@ lightboxOverlay.addEventListener('click', () => {
     modalImg.src = '';
 })
 
-document.addEventListener('keydown', (e) => {
-    if(e.key === 'Escape'){
-    modal.classList.remove('is-open');
-    modalImg.src = '';
+document.addEventListener('keydown', (event) => {
+    if (event.key === 'Escape') {
+        modal.classList.remove('is-open');
+        modalImg.src = '';
+    }
+
+    if (event.key === 'ArrowRight') {
+        nextImage();
+    }
+
+    if (event.key === 'ArrowLeft') {
+        prevImage();
     }
 })
+
+function nextImage() {
+    if (indexOfImg === imgArr.length - 1) {
+        indexOfImg = 0;
+        modalImg.src = imgArr[indexOfImg].original;
+        modalImg.alt = imgArr[indexOfImg].description;
+        return;
+    }
+    
+    indexOfImg += 1;
+    modalImg.src = imgArr[indexOfImg].original;
+    modalImg.alt = imgArr[indexOfImg].description;
+}
+
+function prevImage() {
+    if (indexOfImg === 0) {
+        indexOfImg = imgArr.length - 1;
+        modalImg.src = imgArr[indexOfImg].original;
+        modalImg.alt = imgArr[indexOfImg].description;
+        return;
+    }
+    
+    indexOfImg -= 1;
+    modalImg.src = imgArr[indexOfImg].original;
+    modalImg.alt = imgArr[indexOfImg].description;
+}
